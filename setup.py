b48bfe6579fd9cc709a084da9edc92f7537bd6dc
heuvel <arjanvandenheuvel@godatadriven.com>
2021-03-19 14:53:46 +0100

    test on github

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
 from setuptools import setup, find_packages
 
+base_packages = []
+
+test_packages = ["pytest>=6.2.2", "black>=20.8b1", "flake8>=3.9.0"]
+
+util_packages = ["jupyterlab>=2.2.6"]
+
+dev_packages = test_packages + util_packages
+
+
 setup(
     name="budgetplan",
     version="0.1.0",
     packages=find_packages(),
+    extras_requires={"dev": dev_packages, "test": test_packages},
 )
